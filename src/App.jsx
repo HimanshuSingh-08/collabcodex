af7894853a659926431d110160ff339296f2e408
Himanshu Singh <Himanshu Singh>
2024-03-03 16:13:00 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./Components/Login"
+import Navbar from "./Components/Navbar"
+import EditorPage from "./Components/EditorPage"
 
 function App() {
 
   return (
     <>
-      
+    <h1>hello</h1>
+      <Navbar/>
+      <BrowserRouter>
+       <Routes>
+          <Route path='/login' element={<Login/>} ></Route>
+          <Route path='/editor/:roomid' element={<EditorPage/>} ></Route>
+
+       </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Components/EditorPage.jsx
@@ -0,0 +1,8 @@
+
+export default function EditorPage() {
+  return (
+    <>
+    <h1>this is the editor page </h1>
+    </>
+  )
+}
